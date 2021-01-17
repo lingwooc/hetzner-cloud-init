b82afdbdb20d4a6e1061f17ff46af4980ee2369b
Chris Lingwood <chris@dtecg.co.uk>
2021-01-17 11:24:51 +0000

    allow from ip on all interfaces

--- update-config.sh
+++ update-config.sh
@@ -41,8 +41,7 @@ cp /etc/current_node_ips /etc/old_node_ips
 echo "" > /etc/current_node_ips
 
 for IP in "${NEW_NODE_IPS[@]}"; do
-  /usr/sbin/ufw allow in on ens10 from "$IP"
-  /usr/sbin/ufw delete deny from "$IP"
+  /usr/sbin/ufw allow from "$IP"
   echo "$IP" >> /etc/current_node_ips
 done
 
